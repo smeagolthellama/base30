30851093d3fc9445bb8b95aa4b962dbc02c8e8c7
M.G <magardner2010@gmail.com>
2017-03-28 09:41:24 +0300

    add converter

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    char yn;
+    char str[255];
+    char* e;
+    cout<<"from 30(y) or to 30(n)?\n";
+    cin>>yn;
+    cin>>str;
+    if(yn=='y'){
+        cout<<strtol(str,&e,30);
+    }else{
+        int n=atoi(str);
+        cout<<itoa(n,str,30);
+
+    }
+    return 0;
+}
